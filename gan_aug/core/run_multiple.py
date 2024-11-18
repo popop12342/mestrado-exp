748f9366dffb2b2a837b615882b722e7f79480c7
popop12342 <felipepinna17@gmail.com>
2024-11-18 20:18:07 -0300

    model parametrization on multiple run

--- gan_aug/core/run_multiple.py
+++ gan_aug/core/run_multiple.py
@@ -5,9 +5,10 @@ from gan_textgen_bert import objective
 from bert import train
 
 datasets = [
-    'subj_001', 'subj_005', 'subj_010', 'subj_020', 'subj_030', 'subj_040', 
+    'subj_001', 'subj_005', 'subj_010', 'subj_020', 'subj_030', 'subj_040',
     'subj_050', 'subj_060', 'subj_070', 'subj_080', 'subj_090', 'subj'
 ]
+
 
 class SingleTrial:
     def __init__(self, study, trial_id):
@@ -25,20 +26,27 @@ class SingleTrial:
     def to_dict(self):
         return {'accuracy': self.accuracy, 'step': self.step}
 
-def run_one_experiment(dataset: str, trial_id: int) -> SingleTrial:
+
+def run_one_experiment(dataset: str, trial_id: int, model: str) -> SingleTrial:
     Study = namedtuple('Study', ['user_attrs', 'study_name'])
-    study = Study(defaultdict(str), study_name='gan-textgen-bert-llm')
+    study = Study(defaultdict(str), study_name=f'{model}-llm')
     study.user_attrs['dataset'] = dataset
     study.user_attrs['num_layers'] = 1
     study.user_attrs['num_aug'] = 0
     study.user_attrs['train_aug'] = 0
-    study.user_attrs['hidden_size'] = 512
+    study.user_attrs['hidden_size'] = 1
     trial = SingleTrial(study, trial_id=trial_id)
-    # GAN-TEXTGEN-BERT objective
-    objective(trial)
-    # simple BERT training
-    # train(trial)
+
+    if model == 'gan-textgen-bert':
+        # GAN-TEXTGEN-BERT objective
+        objective(trial)
+    elif model == 'bert':
+        # simple BERT training
+        train(trial)
+    else:
+        raise ValueError('Model is not known')
     return trial
+
 
 def run_compare_experiments():
     results = {'gan-textgen-bert': {}, 'gen-bert': {}}
@@ -48,18 +56,19 @@ def run_compare_experiments():
         trial_count += 1
         results['gan-textgen-bert'][ds] = acc
         print('Endend experiment for dataset ' + ds + ' with accuracy ' + str(acc))
-    
+
     with open('stats/subj/gan-textgen-bert/compare.json', 'w') as f:
         json.dump(results, f)
 
-def run_multiple_times_single_experiment(dataset: str, n: int):
+
+def run_multiple_times_single_experiment(dataset: str, n: int, model: str):
     results = {dataset: []}
     for i in range(n):
-        trial = run_one_experiment(dataset, i)
+        trial = run_one_experiment(dataset, i, model)
         print(f'Best accuracy was {trial.accuracy} on epoch {trial.step}')
         results[dataset].append(trial.to_dict())
 
-    with open(f'stats/llmaclImdb/gan-textgen-bert/{dataset}.json', 'w') as f:
+    with open(f'stats/llmsubj/samples-20_naug-1/{model}/{dataset}.json', 'w') as f:
         json.dump(results, f)
 
 
@@ -67,5 +76,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--dataset', type=str, help='Dataset to run mutltiple times')
     parser.add_argument('--repeat', type=int, help='Number of times to repeat each experiment', default=10)
+    parser.add_argument('--model', type=str, help='Model evaluated (gan-textgen-bert or bert)',
+                        default='gan-textgen-bert', choices=['gan-textgen-bert', 'bert'])
     args = parser.parse_args()
-    run_multiple_times_single_experiment(args.dataset, args.repeat)
+    run_multiple_times_single_experiment(args.dataset, args.repeat, args.model)
