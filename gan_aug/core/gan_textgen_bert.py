35ce5bbff88b136d07ee60ad16578ff12d3fc8c9
popop12342 <felipepinna17@gmail.com>
2025-10-28 22:23:22 -0300

    gan-gpt-bert

--- gan_aug/core/gan_textgen_bert.py
+++ gan_aug/core/gan_textgen_bert.py
@@ -69,8 +69,8 @@ def objective(trial: Trial) -> float:
     discriminator = BERTDiscriminator(num_layers, seq_size, device, num_labels=len(labels))
     generator = Generator(num_layers=4, noise_size=noise_size, output_size=tokenizer.vocab_size,
                           hidden_size=hidden_size, initial_temperature=initial_temp)
-    # print(generator)
-    # print('generator parameters: ' + str(sum(p.numel() for p in generator.parameters() if p.requires_grad)))
+    print(generator)
+    print('generator parameters: ' + str(sum(p.numel() for p in generator.parameters() if p.requires_grad)))
     # print(discriminator)
     # print('discriminator parameters: ' + str(sum(p.numel() for p in discriminator.parameters() if p.requires_grad)))
 
